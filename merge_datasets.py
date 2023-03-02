f1f7bb2295baf4a3c0b3b243ae0082561cc8e4d6
jbunker <jbunker@splunk.com>
2023-03-02 14:49:15 +0000

    Add merge_datasets script

---
+++ merge_datasets.py
@@ -0,0 +1,27 @@
+import json
+
+def save_json(obj, path):
+    with open(path, 'w') as f:
+        json.dump(obj, f, indent=2)
+
+
+def load_json(path):
+    with open(path, 'r') as f:
+        return json.load(f)
+
+
+def merge_datasets(src_paths, dest_path):
+    full_json = {}
+    for src_path in src_paths:
+        full_json |= load_json(src_path)
+    save_json(full_json, dest_path)
+
+
+if __name__ == '__main__':
+    src_paths = [
+        '../../../Desktop/L1/data.json',
+        '../../../Desktop/L1_Additional/data.json',
+        '../../../Desktop/L2/data.json',
+    ]
+    dest_path = '../../../Desktop/pothole_dataset'
+    merge_datasets(src_paths, dest_path)
